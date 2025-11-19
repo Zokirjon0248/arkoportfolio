44a9c7f79ccd4c4e0f26ee32ad6b57deae3a1bfc
Nematov Zokirjon <z.nematov.uz@gmail.com>
2025-11-20 00:09:17 +0500

    headerdagi hato togirlani a dan linkga

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,9 +35,9 @@ const Header = () => {
 
           <div className="hidden md:flex md:items-center md:space-x-2 z-10">
             {navigation.map((item) => (
-              <a
+              <Link
                 key={item.name}
-                href={item.href}
+                to={item.href}
                 className="relative text-gray-100 hover:text-white px-5 py-2.5 rounded-full text-sm font-medium transition-all duration-300 ease-in-out group overflow-hidden"
               >
                 {/* Hover gradient background */}
@@ -51,7 +51,7 @@ const Header = () => {
 
                 {/* Bottom line indicator */}
                 <span className="absolute bottom-1 left-1/2 -translate-x-1/2 w-0 group-hover:w-3/4 h-0.5 bg-gradient-to-r from-cyan-400 to-blue-500 transition-all duration-300 rounded-full"></span>
-              </a>
+              </Link>
             ))}
           </div>
 
